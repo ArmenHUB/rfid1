a28724df0ceb0e10370d84cb418259ffba86005a
ArmenHUB <36188020+ArmenHUB@users.noreply.github.com>
2018-08-24 09:58:09 +0400

    Update rf55.py

--- rf55.py
+++ rf55.py
@@ -22,10 +22,10 @@ GPIO.setup(green, GPIO.OUT, initial=0) # тоже самое с пином 11
 continue_reading = True
 
 def end_read(signal,frame): # что делать, если программу прервать и как её прервать
-    global continue_reading
-    print "Ctrl+C captured, ending read."
-    continue_reading = False
-    GPIO.cleanup()
+	global continue_reading
+	print "Ctrl+C captured, ending read."
+	continue_reading = False
+	GPIO.cleanup()
 
 # Create an object of the class MFRC522 (??)
 MIFAREReader = MFRC522.MFRC522()
@@ -33,53 +33,53 @@ MIFAREReader = MFRC522.MFRC522()
 while continue_reading:
 
 # Сканируем карты - считываем их UID
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+	(status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-    # Если карту удалось считать, пишем "карта найдена"
-    if status == MIFAREReader.MI_OK:
-        print "Card detected"
+	# Если карту удалось считать, пишем "карта найдена"
+	if status == MIFAREReader.MI_OK:
+		print "Card detected"
 
-    # Считываем UID карты
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+	# Считываем UID карты
+	(status,uid) = MIFAREReader.MFRC522_Anticoll()
 
-    # Если считали UID, то идем дальше
-    if status == MIFAREReader.MI_OK:
-        # выводим UID карты на экран
-        UIDcode = str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3])
-        print UIDcode
+	# Если считали UID, то идем дальше
+	if status == MIFAREReader.MI_OK:
+		# выводим UID карты на экран
+		UIDcode = str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3])
+		print UIDcode
 
-        mydb = mysql.connector.connect(
-           host="10.11.1.188",
-           user="root",
-           passwd="test1234",
-           database="Door"
-        )
-        mycursor = mydb.cursor()
-        
-       sql = "SELECT UID FROM cards WHERE UID =  %s"
-       val = (UIDcode)
+		mydb = mysql.connector.connect(
+		   host="10.11.1.188",
+		   user="root",
+		   passwd="test1234",
+		   database="Door"
+		)
+		mycursor = mydb.cursor()
+		
+	   sql = "SELECT UID FROM cards WHERE UID =  %s"
+	   val = (UIDcode)
 
-       rows_count =  mycursor.execute(sql, val)        
-        # Если карта есть в списке
-         if rows_count > 0:
-        # то дверь открывается
-        # предполагается, что замок открывается при подаче на
-        # него (на реле, управляющее замком), напряжения
-        # т.е. им управляет переключаемое реле
-        # т.е. замок открывается при высоком значении пина doorlock
-        # при этом, горит зеленая, тухнет красная и пищит динамик
+	   rows_count =  mycursor.execute(sql, val)        
+		# Если карта есть в списке
+		 if rows_count > 0:
+		# то дверь открывается
+		# предполагается, что замок открывается при подаче на
+		# него (на реле, управляющее замком), напряжения
+		# т.е. им управляет переключаемое реле
+		# т.е. замок открывается при высоком значении пина doorlock
+		# при этом, горит зеленая, тухнет красная и пищит динамик
 
-                GPIO.output((green), (1))
-                print "Door open"
+				GPIO.output((green), (1))
+				print "Door open"
 
-                # успеть дернуть за 1 секунду
-                time.sleep(1)
-                GPIO.output((green), (0))
+				# успеть дернуть за 1 секунду
+				time.sleep(1)
+				GPIO.output((green), (0))
 
-                # потом дверь закрывается, о чем нас извещают
-                print "Door closed"
+				# потом дверь закрывается, о чем нас извещают
+				print "Door closed"
 
-        # А если карты в списке нет, то моргаем и пищим
-        else:
-                GPIO.output((green), (0))
-                print "Unrecognised Card"
+		# А если карты в списке нет, то моргаем и пищим
+		else:
+				GPIO.output((green), (0))
+				print "Unrecognised Card"
